e09e7a61eb8d15db5f922890b4dbc5a0b8fb26d2
carrt3rr <139090201+carrt3rr@users.noreply.github.com>
2023-07-09 22:33:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// JavaScript code can be added here if needed
+window.addEventListener('DOMContentLoaded', () => {
+  const ball = document.getElementById('ball');
+  const containerWidth = window.innerWidth;
+  const containerHeight = window.innerHeight;
+  const ballWidth = ball.offsetWidth;
+  const ballHeight = ball.offsetHeight;
+
+  let x = containerWidth / 2 - ballWidth / 2; // initial x position
+  let y = containerHeight / 2 - ballHeight / 2; // initial y position
+  let dx = 2; // horizontal velocity
+  let dy = 2; // vertical velocity
+
+  function animate() {
+    // Update ball position
+    x += dx;
+    y += dy;
+
+    // Check collision with walls
+    if (x + ballWidth >= containerWidth || x <= 0) {
+      dx = -dx; // Reverse horizontal velocity
+    }
+    if (y + ballHeight >= containerHeight || y <= 0) {
+      dy = -dy; // Reverse vertical velocity
+    }
+
+    // Move the ball
+    ball.style.transform = `translate(${x}px, ${y}px)`;
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+});
+
